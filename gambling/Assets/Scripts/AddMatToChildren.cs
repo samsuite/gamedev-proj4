3894e331b345fccf68090d7c446d5abcbc50b5e9
wrighp <wrightcoder@gmail.com>
2016-12-07 11:04:52 -0500

    addMatToChildren in a mesh

---
+++ gambling/Assets/Scripts/AddMatToChildren.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AddMatToChildren : MonoBehaviour {
+	public Material material;
+	// Use this for initialization
+	void Start () {
+		MeshRenderer[] oldMats = GetComponentsInChildren<MeshRenderer> ();
+		for (int i = 0; i < oldMats.Length; i++) {
+			Material mat = new Material (material);
+			Material oldMat = oldMats [i].material;
+			mat.CopyPropertiesFromMaterial (oldMat);
+			oldMats [i].material = mat;
+			//mat.color = oldMat.color;
+			//mat.SetColor("_EmissionoldMat.GetColor("_Emission")
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
