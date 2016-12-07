ec07823f5f3d46226bedb82f8e4b900f3c37e5dc
samsuite <suitester@gmail.com>
2016-12-07 11:44:07 -0500

    card pickup functionality

--- gambling/Assets/Scripts/GameManager.cs
+++ gambling/Assets/Scripts/GameManager.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
+
 
 public class GameManager : MonoBehaviour {
 
@@ -7,10 +9,18 @@ public class GameManager : MonoBehaviour {
 
     public static GameObject player;
     public static GameObject camera;
-    public static int money = 0;
+    public static int money = 150;
+    public static int cards_left = 0;
     public static int day = 0;
     public static bool can_walk = true;
 
+    bool showing_spent = false;
+    float spent_timer = 0f;
+
+    public Text money_text;
+    
+    public GameObject spent_image;
+    public Text spent_text;
     static GameObject current_card;
     static int prize_money;
 
@@ -18,21 +28,52 @@ public class GameManager : MonoBehaviour {
 
     public GameObject card_inp;
     public static GameObject card;
+    public Button done_button_inp;
+    public static Button done_button;
 
 	void Awake () {
 	    player = GameObject.FindGameObjectWithTag("Player");
         camera = Camera.main.gameObject;
         card = card_inp;
+        done_button = done_button_inp;
+
+        done_button.gameObject.SetActive(false);
+        spent_image.SetActive(false);
 	}
 
-    void LateUpdate () {
-        if (!made){
-            scratch_card(10, 6);
-            made = true;
+
+    void Update () {
+        money_text.text = "$"+money.ToString();
+
+        if (showing_spent){
+            spent_timer -= Time.deltaTime;
+            if (spent_timer <= 0f){
+                showing_spent = false;
+                spent_image.SetActive(false);
+            }
         }
     }
 
-    public void scratch_card (int price, int value) {
+    /*
+    //// delete this later
+    void LateUpdate () {
+        if (!made){
+            scratch_card(10, 6);
+            buy_cards (50);
+            made = true;
+        }
+    }
+    */
+
+
+    public void buy_cards (int amount_spent) {
+        spent_timer = 2f;
+        showing_spent = true;
+        spent_image.SetActive(true);
+        spent_text.text = "YOU SPENT $"+amount_spent+" ON SCRATCH CARDS";
+    }
+
+    public static void scratch_card (int price, int value) {
         prize_money = value;
         money -= price;
         
@@ -45,12 +86,14 @@ public class GameManager : MonoBehaviour {
         card_class.GenerateCard(price, value);
 
         current_card.transform.localScale = new Vector3(0.7f,0.7f,0.7f);
+        done_button.gameObject.SetActive(true);
 
         can_walk = false;
     }
 
     public void card_done () {
         money += prize_money;
+        done_button.gameObject.SetActive(false);
         
         Destroy(current_card);
         can_walk = true;
