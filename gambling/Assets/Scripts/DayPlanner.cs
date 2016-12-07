c7f17c7b7d924513752d11b5c68a960649775557
samsuite <suitester@gmail.com>
2016-12-07 12:28:17 -0500

    working on planner

---
+++ gambling/Assets/Scripts/DayPlanner.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DayPlanner : MonoBehaviour {
+
+    [System.Serializable]
+    public struct DayData {
+        public int money_spent;
+        public int cards_bought;
+    }
+
+    public List<DayData> events = new List<DayData>();
+
+	void Start () {
+	    
+
+	}
+
+
+    void add_event (int spent, int num_cards) {
+        DayData new_event = new DayData();
+        new_event.money_spent = spent;
+        new_event.cards_bought = num_cards;
+    }
+}
